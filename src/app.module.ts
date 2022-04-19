c2406fe23d7d18953a2621dca2dac950fb4c8360
OmkoBass <omkob@hotmail.com>
2022-04-19 22:30:10 +0200

    add all modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    PostModule,
+    AuthModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
